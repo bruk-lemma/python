6816cff154cbdcad77fccb93ec3c7cf72436834f
Bruk Lemma <bruklemma2017@gmail.com>
2022-06-29 14:02:47 +0300

    created automated dark mode selector test selenium file

--- search_client_selenium.py
+++ search_client_selenium.py
@@ -10,7 +10,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def login(email, password):
+def serach_bar(name):
     driver = webdriver.Chrome(
         '/home/bruk/Downloads/Compressed/chromedriver_linux64_1/chromedriver')
     driver.get("http://3.82.160.140/vms/")
@@ -18,12 +18,23 @@ def login(email, password):
     elem2 = driver.find_element(By.ID, ("login-password"))
     button = driver.find_element(By.CLASS_NAME, ("btn"))
     elem.clear()
-    elem.send_keys(email)
-    elem2.send_keys(password)
-    # button.click()
-    for e in driver.get_log('browser'):
-        print(e)
+    elem.send_keys("2511111111")
+    elem2.send_keys("123456")
     button.click()
+    driver.implicitly_wait(10)
+    avatar = driver.find_element(By.CLASS_NAME, ("nav-item"))
+    avatar.click()
+    users = driver.find_element(By.LINK_TEXT, ("Users"))
+    users.click()
+    clients = driver.find_element(By.LINK_TEXT, ("Clients"))
+    clients.click()
+    driver.implicitly_wait(10)
+   # addnewclient = driver.find_element(By.CLASS_NAME, ("add-new"))
+   # addnewclient.click()
+
+    WebDriverWait(driver, 50).until(EC.element_to_be_clickable(
+        (By.XPATH, "/html/body/div[3]/div[3]/div[2]/div/div[1]/div/div[1]/div[2]/div/label/input"))).send_keys(name)
 
 
-login("2511111111", "123456")
+serach_bar("senay")
+serach_bar("joe")
--- security_register_test_selenium.py
+++ security_register_test_selenium.py
@@ -10,6 +10,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
+# def create_customer():
 driver = webdriver.Chrome(
     '/home/bruk/Downloads/Compressed/chromedriver_linux64_1/chromedriver')
 driver.get("http://3.82.160.140/vms/")
@@ -25,11 +26,9 @@ avatar = driver.find_element(By.CLASS_NAME, ("nav-item"))
 avatar.click()
 users = driver.find_element(By.LINK_TEXT, ("Users"))
 users.click()
-clients = driver.find_element(By.LINK_TEXT, ("Clients"))
+clients = driver.find_element(By.LINK_TEXT, ("Security"))
 clients.click()
 driver.implicitly_wait(10)
-WebDriverWait(driver, 50).until(EC.element_to_be_clickable(
-    (By.XPATH, "/html/body/nav/div/ul/li[2]"))).click()
 addnewclient = driver.find_element(By.CLASS_NAME, ("add-new"))
 addnewclient.click()
 
@@ -38,15 +37,23 @@ phone = driver.find_element(By.ID, ("user_contact"))
 email = driver.find_element(By.ID, ("email"))
 address = driver.find_element(By.ID, ("address"))
 password = driver.find_element(By.ID, ("password"))
+driver.implicitly_wait(10)
+parent = driver.find_element(By.CLASS_NAME, ("form-control"))
+driver.implicitly_wait(10)
 submit = driver.find_element(By.ID, ("submit_new"))
-driver.implicitly_wait(30)
 
-fullname.send_keys("cavani")
-phone.send_keys("4578129638")
-email.send_keys("cavani@gmail.com")
-address.send_keys("gonder")
-password.send_keys("123456")
-address.send_keys("")
-driver.implicitly_wait(30)
+fullname.send_keys("vidic")
+phone.send_keys("4242424245")
+email.send_keys("vidic@gmail.com")
+address.send_keys("bahirdar")
+password.send_keys("123456789")
+driver.implicitly_wait(20)
+driver.implicitly_wait(10)
+WebDriverWait(driver, 50).until(EC.element_to_be_clickable(
+    (By.XPATH, "/html/body/div[3]/div[3]/div[2]/div/div[3]/div/form/div[2]/div[8]/select"))).click()
+WebDriverWait(driver, 50).until(EC.element_to_be_clickable(
+    (By.XPATH, "//html/body/div[3]/div[3]/div[2]/div/div[3]/div/form/div[2]/div[8]/select/option[5]"))).click()
+address.send_keys(" ")
 
-# submit.click()
+submit.click()
+# create_customer()
